02f7831b96a3c905d455a1fe6dccd558a9408a38
fred <domaine75@gmail.com>
2018-12-10 17:36:32 +0100

    Add files

--- exo/day1/sites/jquery/js/myjquery1.js
+++ exo/day1/sites/jquery/js/myjquery1.js
@@ -1,1 +1,22 @@
+function affiche(message){
+    alert(message);
+}
+
+function wait(temps){
+    setTimeout(() => {
+        
+    }, temps);
+}
+
+$(document).ready(function() {
+    //affiche("hello");
+    //affiche("world");
+    console.log($('#content'));
+    wait(5000);
+    $('#content').html('<h1>jQuery est trop facile</h1>');
+    $('#content').prepend('texte inséré avant');
+    $('#content').append('texte inséré après');
+});
+
 console.log($);
+console.log("test pour debug");
---
+++ exo/day1/sites/jquery/js/myjquery2.js
@@ -0,0 +1,11 @@
+function affiche(message){
+    alert(message);
+}
+$(document)
+$(document).ready(function() {
+    affiche("hello");
+    affiche("world");
+});
+
+console.log($);
+console.log("test pour debug");
