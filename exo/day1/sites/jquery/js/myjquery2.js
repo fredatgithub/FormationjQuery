456245ef9f5dd0a7dce4060cba59e60d43d6cae4
fred <domaine75@gmail.com>
2018-12-11 11:43:20 +0100

    Add files

--- exo/day1/sites/jquery/js/myjquery2.js
+++ exo/day1/sites/jquery/js/myjquery2.js
@@ -5,12 +5,25 @@ $(document).ready(function() {
 
     let varBoolean = true; // let depuis ECMA script6
     let test2 = 555;
-    
+    let test3 = '99'; 
+    if(test3 == 99){
+        console.log('test3 vaut : ' + test3);
+        console.log('le type de test3 vaut : ' + $.type(test3));
+    }
+
+
+    if(test2 === 555){ // === valeur et type plus rapide car pas de conversion avant test, type cohersion
+        var toto = 1; // visible dans la portée globale
+        let titi = 33; // visible dans le contexte local, sera supprimé en dehors de if 
+    }
 
     console.log(varNumber);
     console.log($.type(varNumber));
 
     console.log(varText);
     console.log($.type(varText));
+
+    console.log(toto);
+    //console.log(titi); // erreur variable non définie dans le contexte
 });
 
