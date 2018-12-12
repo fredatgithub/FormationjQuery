292747f4dff70ebdc3c7b149a6e53f7ceb187956
fred <domaine75@gmail.com>
2018-12-12 12:15:49 +0100

    Add files

--- exo/day1/sites/jquery/js/accordeon.js
+++ exo/day1/sites/jquery/js/accordeon.js
@@ -3,8 +3,9 @@ j(document).ready(function() {
 
 j('.soufflet').hide();
 //.on est un gestionnaire d'évènement
-j('#accordeon p').on('click', function(){
+j('#accordeon p').on('click', function(event){
     console.log('click ok');
+    console.log(event);
     });
 
     
